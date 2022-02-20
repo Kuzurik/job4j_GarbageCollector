cac2e7db5ac1c81216b350d08026e42f6659df6e
Alex <vo>
2022-02-20 12:35:10 +0300

    1. Реализация кеша на SoftReference [#1592 #266475]

---
+++ src/main/java/ru/job4j/cache/Emulator.java
@@ -0,0 +1,41 @@
+package ru.job4j.cache;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Emulator {
+
+    private String key;
+    private AbstractCache<String, String> cache;
+
+    public void getPath() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter directory for caching");
+        this.cache = new DirFileCache(scanner.nextLine());
+        System.out.println("enter cache file");
+        this.key = scanner.nextLine();
+
+    }
+
+    public void loadFile() throws IOException {
+        if (!cache.isKey(key)) {
+            cache.put(key, cache.load(key));
+        }
+    }
+
+    public String getFromCash(String key) {
+        return cache.get(key);
+    }
+
+
+
+    public static void main(String[] args) throws IOException {
+        Emulator em = new Emulator();
+        em.getPath();
+        em.loadFile();
+        System.out.println(em.getFromCash("Rihanna.txt"));
+
+
+    }
+}
